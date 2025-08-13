176a7df48fd8ab57e9c78b49cd18539d01eac5ec
unknown <lab1062@etecjk.local>
2025-08-12 22:25:18 -0300

    aula criação de obj

---
+++ pessoaobj.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            pessoa objJogador;//criei uma "pessoa"
+            pessoa camisa7;
+            objJogador = new pessoa();
+            camisa7 = new pessoa();
+            objJogador.cadastrar()
+            camisa7.cadastrar();
+            objJogador.exibir();
+            camisa7.exibir();
+            
+        }
+    }
+}
